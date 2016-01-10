ab40746d61b6dabe3ff674d4ed1ae32ce4fcc026
linusekdahl <linusekdahl@gmail.com>
2016-01-10 19:17:18 +0100

    Next Commit

--- Exercise2/MenuPage2.js
+++ Exercise2/MenuPage2.js
@@ -1,4 +1,5 @@
 ﻿/*
+
 1. create 7 hamburger objects using literal notation 
    use attributes name, size, dayOfTheWeek and price(page. 104 in book)
    Please use the same html syntax of an article as in exercise1.
@@ -26,6 +27,7 @@ var p = 1;
 
 
 /*
+
 2. create Product objects with constructor notation, 
    use the parameters name, size, price, dayOfTheWeek  (page. 108 in book)
 */
@@ -54,35 +56,33 @@ function Sub(name, dayOfTheWeek, size, price) {
 
 /*
 
-
 3. add 7 drinks to the page using Product.
 */
 
 var drinkList = [];
 
-drinkList.push(new Drink('Super Drink', 'monday', small, 60))
-drinkList.push(new Drink('Naughty Drink', 'thuseday', small, 20))
-drinkList.push(new Drink('Unhealthy Drink', 'wednesday', medium, 80))
-drinkList.push(new Drink('ThatOtherDay Drink', 'thursday', medium, 30))
-drinkList.push(new Drink('Bad Drink', 'friday', medium, 20))
-drinkList.push(new Drink('Good Drink', 'saturday', large, 70))
-drinkList.push(new Drink('GetWasted Drink', 'sunday', large, 100))
+drinkList.push(new Drink('Super Drink', 'monday', 'small', 60))
+drinkList.push(new Drink('Naughty Drink', 'thuseday', 'small', 20))
+drinkList.push(new Drink('Unhealthy Drink', 'wednesday', 'medium', 80))
+drinkList.push(new Drink('ThatOtherDay Drink', 'thursday', 'medium', 30))
+drinkList.push(new Drink('Bad Drink', 'friday', 'medium', 20))
+drinkList.push(new Drink('Good Drink', 'saturday', 'large', 70))
+drinkList.push(new Drink('GetWasted Drink', 'sunday', 'large', 100))
 
 /*
-
 
 4. add 7 pizzas to the page using Product.
 */
 
 var PizzaList = [];
 
-PizzaList.push(new Pizza('Margaretta Pizza', 'monday', small, 50))
-PizzaList.push(new Pizza('Tropicani Pizza', 'thuseday', small, 60))
-PizzaList.push(new Pizza('Venezuela Pizza', 'wednesday', medium, 70))
-PizzaList.push(new Pizza('Kebab Pizza', 'thursday', medium, 20))
-PizzaList.push(new Pizza('Slimy Onion Pizza', 'friday', small, 40))
-PizzaList.push(new Pizza('Karlsson Pizza', 'saturday', large, 40))
-PizzaList.push(new Pizza('Äcklig Pizza', 'sunday', large, 20))
+PizzaList.push(new Pizza('Margaretta Pizza', 'monday', 'small', 50))
+PizzaList.push(new Pizza('Tropicani Pizza', 'thuseday', 'small', 60))
+PizzaList.push(new Pizza('Venezuela Pizza', 'wednesday', 'medium', 70))
+PizzaList.push(new Pizza('Kebab Pizza', 'thursday', 'medium', 20))
+PizzaList.push(new Pizza('Slimy Onion Pizza', 'friday', 'small', 40))
+PizzaList.push(new Pizza('Karlsson Pizza', 'saturday', 'large', 40))
+PizzaList.push(new Pizza('Äcklig Pizza', 'sunday', 'large', 20))
 
 /*
 
--- SearchIntegration/$cripts/JavaScript.js
+++ SearchIntegration/$cripts/JavaScript.js
@@ -1,4 +1,38 @@
-﻿/* <h1 id ="ad">Menu!</h1>
+﻿
+
+var h1Basic = document.createElement("H1");
+h1Basic.setAttribute("id", "ad");
+h1Basic.innerText = "Menu!";
+
+
+var h2Basic = document.createElement("H2");
+h2Basic.innerText = "Delicious Burgers";
+
+var articleBasic = document.createElement("article");
+var fieldSet = document.createElement("fieldset");
+
+var legend = document.createElement("legend");
+var h3 = document.createElement("h3");
+var span = document.createElement("span");
+
+var div = document.createElement("div");
+div.setAttribute("class", "price");
+div.setAttribute("id", "monday");
+
+$('body').append(h1Basic);
+$('body').append(h2Basic);
+
+$('body').append(articleBasic);
+$(articleBasic).append(fieldSet);
+
+$(fieldSet).append(legend);
+$(fieldSet).append(h3);
+$(fieldSet).append(span);
+$(fieldSet).append(div);
+
+var h2Basic = document.createElement("H2");
+h2Basic.innerText = "Apa";
+/* <h1 id ="ad">Menu!</h1>
 <h2 >Delicious burgers</h2>
 <article>
     <fieldset>
@@ -9,4 +43,4 @@
         */
 
 
-var divBasic = document.createElement("H1");
+var x = 1;

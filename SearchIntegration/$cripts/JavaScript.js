24b77a6bca9b205d193fd2695470c9a2905e7f66
linusekdahl <linusekdahl@gmail.com>
2016-01-10 17:16:41 +0100

    commit

--- SearchIntegration/$cripts/JavaScript.js
+++ SearchIntegration/$cripts/JavaScript.js
@@ -1,1 +1,12 @@
-﻿
+﻿/* <h1 id ="ad">Menu!</h1>
+<h2 >Delicious burgers</h2>
+<article>
+    <fieldset>
+        <legend>Monday Burger</legend>
+        <h3>Pepper burger</h3>
+        <span>No image loaded</span>
+        <div class="price" id="monday">19.90</div>
+        */
+
+
+var divBasic = document.createElement("H1");

940334b72212229f1ebbcb0e713b8d9b843f8d7e
linusekdahl <linusekdahl@gmail.com>
2016-01-10 13:21:48 +0100

    Menupage2 Goodies

--- Exercise2/MenuPage2.js
+++ Exercise2/MenuPage2.js
@@ -2,12 +2,84 @@
 1. create 7 hamburger objects using literal notation 
    use attributes name, size, dayOfTheWeek and price(page. 104 in book)
    Please use the same html syntax of an article as in exercise1.
+*/
+
+function Burger(name, dayOfTheWeek, size, price) {
+    this.name = name;
+    this.dayOfTheWeek = dayOfTheWeek;
+    this.size = size;
+    this.price = price;
+}
+
+var burgerList = [];
+
+burgerList.push(new Burger('Amazing Burger', 'monday', 200, 50))
+burgerList.push(new Burger('Mega Burger', 'thuseday', 250, 60))
+burgerList.push(new Burger('GetFat Burger', 'wednesday', 500, 70))
+burgerList.push(new Burger('Slimy Burger', 'thursday', 100, 20))
+burgerList.push(new Burger('Elite Burger', 'friday', 150, 40))
+burgerList.push(new Burger('Conda Burger', 'saturday', 50, 10))
+burgerList.push(new Burger('Milena Burger', 'sunday', 100, 20))
+
+var p = 1;
+
+
+
+/*
 2. create Product objects with constructor notation, 
    use the parameters name, size, price, dayOfTheWeek  (page. 108 in book)
+*/
+
+function Drinks(name, dayOfTheWeek, size, price) {
+    this.name = name;
+    this.dayOfTheWeek = dayOfTheWeek;
+    this.size = size;
+    this.price = price;
+}
+
+function Pizzas(name, dayOfTheWeek, size, price) {
+    this.name = name;
+    this.dayOfTheWeek = dayOfTheWeek;
+    this.size = size;
+    this.price = price;
+}
+
+
+/*
+
+
 3. add 7 drinks to the page using Product.
+*/
+
+
+
+/*
+
+
 4. add 7 pizzas to the page using Product.
+*/
+
+
+
+/*
+
+
 5. add 7 subs to the page using Product.
+*/
+
+
+
+/*
+
+
 6. GO to file FixIt.html
+*/
+
+
+
+/*
+
+
 */
 //DRINKS
 //Subs
